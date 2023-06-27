f58bc3ed2b38cbeea542ada043f85c20a7d50623
H3c7o4 <hectorvladitok@gmail.com>
2023-06-27 17:49:00 +0200

    Create the 6-final-user.js project

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from "./utils"
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-	.then((values) => {
-	  const { body } = values[0];
-	  const { firstname, lastname } = values[1];
-	  console.log(`${body} ${firstname} ${lastname}`);
-	})
-	.catch(() => console.log('Signup system offline'));
+    .then((values) => {
+      const { body } = values[0];
+      const { firstname, lastname } = values[1];
+      console.log(`${body} ${firstname} ${lastname}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
