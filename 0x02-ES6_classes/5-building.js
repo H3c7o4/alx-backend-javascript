e1a0412323c2e883c7e4b4bcb9529b31b2e35f0e
H3c7o4 <hectorvladitok@gmail.com>
2023-06-29 12:22:27 +0100

    Finish the 0x10-ES6_classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
