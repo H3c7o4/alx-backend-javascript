d9f0a27b41613398b5851aaa6c0342cc9fcc3a30
H3c7o4 <hectorvladitok@gmail.com>
2023-08-22 12:06:51 +0200

    Fix bugs in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
-process.stdin.setEncoding('utf8');
-
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', function () {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write('Your name is: ' + name);
+    process.stdout.write('Your name is: ' + name.toString());
   }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
