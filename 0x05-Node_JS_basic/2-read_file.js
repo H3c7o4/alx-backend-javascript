e26c579a4b97e1d062b6ad510a9764a62d9fb79d
H3c7o4 <hectorvladitok@gmail.com>
2023-08-22 11:59:41 +0200

    Add the countStudents function

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,39 @@
 const fs = require('fs');
-const csv = require('csv-parser');
 
-function countStudents(csvFile) {
+const countStudents = (path) => {
+  let data;
   try {
-    const data = [];
-    fs.createReadStream(csvFile)
-      .pipe(
-        parse({
-          delimiter: ",",
-	  columns: true,
-	  ltrim: true,
-	})
-      )
-      .on("data", function(row) {
-        data.push(row);
-      })
-    console.log(`Number of students: ${data.length}`);
+    data = fs.readFileSync(path,
+      { encoding: 'utf8', flag: 'r' });
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-  catch (error) {
-    throw new Error("Cannot load the database")
+
+  data = data.split('\n');
+  let students = data.filter((student) => student);
+  students = students.map((item) => item.split(','));
+
+  const studentSize = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${studentSize}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
+    }
   }
-}
+
+  delete fields.field;
+
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
+    );
+  }
+};
 
 module.exports = countStudents;
