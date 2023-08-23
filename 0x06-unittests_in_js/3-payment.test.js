59d4517b6d7bed5212f0516a0eb4fe06f2260779
H3c7o4 <hectorvladitok@gmail.com>
2023-08-23 16:56:40 +0200

    using sinon.spy to make sure the math used for sendPaymentRequestToApi(100, 20) is the same as Utils.calculateNumber('SUM', 100, 20)

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,8 +4,8 @@ const sinon = require('sinon');
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("sendPaymentRequestToApi", function () {
-  it("should verify utils.calculateNumber is called once", function () {
+describe("#sendPaymentRequestToApi", function () {
+  it("should call utils.calculateNumber", function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(Utils.calculateNumber.withArgs('SUM', 100, 20).calledOnce).to.be.true;
