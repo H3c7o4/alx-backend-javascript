8899403274ca51ac50b383e282b0c7d47a769f92
H3c7o4 <hectorvladitok@gmail.com>
2023-08-24 18:03:10 +0200

    Test the api.js suite

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -19,7 +19,6 @@ describe("Index page", function() {
 	    done();
 	});
     });
-});
 
 describe("Cart page", function() {
     it("check correct status code for correct url", function(done) {
@@ -28,11 +27,7 @@ describe("Cart page", function() {
 	    done();
 	});
     });
-    it("check correct content for correct url", function(done) {
-	request.get("http://localhost:7865/cart/12", function(err, res, body) {
-	    expect(body).to.contain("Payment methods for cart 12");
-	    done();
-	});
+
     });
     it("check correct status code for incorrect url", function(done) {
 	request.get("http://localhost:7865/cart/kim", function(err, res, body) {
